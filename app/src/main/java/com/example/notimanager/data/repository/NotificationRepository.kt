80e9ad51ed5d0074eb1d64e8f6e8410deeaa15d0
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-20 16:52:56 +0900

    feat: 데이터 모델 개선
    intent의 경우 직렬화하여 보관하고,
    intentModel을 metaModel로 변경하여,
    알림에 대한 메타데이터를 저장하도록 개선함.

--- app/src/main/java/com/example/notimanager/data/repository/NotificationRepository.kt
+++ app/src/main/java/com/example/notimanager/data/repository/NotificationRepository.kt
@@ -1,30 +1,30 @@
 package com.example.notimanager.data.repository
 
 import android.content.Intent
-import com.example.notimanager.data.model.NotificationIntentModel
+import com.example.notimanager.data.model.NotificationMetaModel
+import com.example.notimanager.data.model.NotificationModel
 import com.example.notimanager.data.source.local.dao.NotificationDao
-import com.example.notimanager.data.source.local.dao.NotificationIntentDao
-import com.example.notimanager.data.utils.ModelConverter.toModel
+import com.example.notimanager.data.source.local.dao.NotificationMetaDao
 import com.example.notimanager.data.utils.ModelConverter.toDomain
 import com.example.notimanager.domain.model.Notification
 import com.example.notimanager.domain.repository.NotificationRepositoryInterface
 
 class NotificationRepository(
     private val notificationDao: NotificationDao,
-    private val notificationIntentDao: NotificationIntentDao
+    private val notificationMetaDao: NotificationMetaDao
 ) : NotificationRepositoryInterface {
-    override suspend fun addNotification(notification: Notification): Long {
-        val model = notification.toModel()
-        val notificationId = notificationDao.insert(model)
-        val intentModel = NotificationIntentModel.fromIntent(notification.intent, notificationId)
-
-        return notificationIntentDao.insert(intentModel)
-    }
-
     override suspend fun getAllNotifications(): List<Notification> {
         return notificationDao.getAll()
             .asSequence()
             .map { it.toDomain(Intent()) }
             .toList()
+    }
+
+    suspend fun insertNotification(notificationModel: NotificationModel): Long{
+        return notificationDao.insert(notificationModel)
+    }
+
+    suspend fun insertNotificationMeta(metaModel: NotificationMetaModel): Long{
+        return notificationMetaDao.insert(metaModel)
     }
 }
