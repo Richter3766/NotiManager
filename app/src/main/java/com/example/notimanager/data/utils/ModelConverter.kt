2746528d25355aa6c0ca3284b63208ec66ea7a0c
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-20 17:12:47 +0900

    refactor: ModelConverter
    해당 클래스를 삭제해도 되지만,
    아직 도메인 계층의 동작을 전부 수정하지 않아,
    당장 불필요한 부분만 삭제함.

--- app/src/main/java/com/example/notimanager/data/utils/ModelConverter.kt
+++ app/src/main/java/com/example/notimanager/data/utils/ModelConverter.kt
@@ -1,24 +1,13 @@
 package com.example.notimanager.data.utils
 
 import android.content.Intent
-import com.example.notimanager.common.objects.JsonMapper.objectMapper
-import com.example.notimanager.data.model.NotificationIntentModel
 import com.example.notimanager.data.model.NotificationModel
 import com.example.notimanager.domain.model.Notification
 
 object ModelConverter {
-    fun Notification.toModel(): NotificationModel {
-        return NotificationModel(
-            packageName = this.packageName,
-            title = this.title,
-            content = this.content,
-            timestamp = this.timestamp,
-        )
-    }
-
     fun NotificationModel.toDomain(intent: Intent): Notification {
         return Notification(
-            packageName = this.packageName,
+            packageName = this.appName,
             title = this.title,
             content = this.content,
             timestamp = this.timestamp,
