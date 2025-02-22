4ec0712e351392aef51d31fdf4b45f01b66fc9f9
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-22 13:58:31 +0900

    feat: add 아이콘 저장 로직
    알림에서 아이콘을 가져와 저장함.
    코루틴을 통해 빠른 성능으로 쓰기 작업을 하도록 함.

--- app/src/main/java/com/example/notimanager/data/service/NotiListenerService.kt
+++ app/src/main/java/com/example/notimanager/data/service/NotiListenerService.kt
@@ -2,17 +2,24 @@ package com.example.notimanager.data.service
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import com.example.notimanager.data.model.AppIconModel
+import com.example.notimanager.data.model.NotificationIconModel
 import com.example.notimanager.data.model.NotificationMetaModel
 import com.example.notimanager.data.model.NotificationModel
-import com.example.notimanager.data.repository.NotificationRepository
+import com.example.notimanager.data.repository.NotificationRepositoryInterface
+import com.example.notimanager.data.utils.AppIconGetter.getAppIconResId
 import com.example.notimanager.data.utils.NameGetter
 import com.example.notimanager.data.utils.PendingIntentHelper
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NotiListenerService: NotificationListenerService() {
-    lateinit var notificationRepository: NotificationRepository
+    @Inject
+    lateinit var notificationRepository: NotificationRepositoryInterface
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         sbn?.let {
@@ -23,22 +30,44 @@ class NotiListenerService: NotificationListenerService() {
             val pendingIntent = notification.contentIntent
             val intentArray = pendingIntent?.let { pi -> PendingIntentHelper.savePendingIntent(pi) }
 
+            val appName = NameGetter.getAppName(this, it)
             val notificationModel = NotificationModel(
-                appName = NameGetter.getAppName(this, it),
+                appName = appName,
                 title = title,
                 content = content,
                 timestamp = postTime
             )
 
+            val notificationIconResId = notification.smallIcon.resId
+            val appIconResId = getAppIconResId(this, it.packageName)
+
             CoroutineScope(Dispatchers.IO).launch {
                 val id = notificationRepository.insertNotification(notificationModel)
-                val notificationMetaModel = NotificationMetaModel(
-                    notificationId = id,
-                    intentActive = true,
-                    intentArray = intentArray ?: "".toByteArray()
-                )
-                notificationRepository.insertNotificationMeta(notificationMetaModel)
+
+                launch {
+                    val notificationMetaModel = NotificationMetaModel(
+                        notificationId = id,
+                        intentActive = true,
+                        intentArray = intentArray ?: "".toByteArray()
+                    )
+                    notificationRepository.insertNotificationMeta(notificationMetaModel)
+                }
+                launch {
+                    val notificationIconModel = NotificationIconModel(
+                        notificationId = id,
+                        notificationIconResId = notificationIconResId
+                    )
+                    notificationRepository.insertNotificationIcon(notificationIconModel)
+                }
+                launch {
+                    val appIconModel = AppIconModel(
+                        appIconResId = appIconResId,
+                        notiAppName = appName
+                    )
+                    notificationRepository.insertAppIcon(appIconModel)
+                }
             }
         }
     }
 }
+
