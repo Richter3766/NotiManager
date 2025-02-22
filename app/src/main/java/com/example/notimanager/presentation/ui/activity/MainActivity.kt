8ed6dfa66eabb54401adf1119e9b70b210b479df
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-22 10:25:02 +0900

    refactor: 알림 허용 UI
    Main 파일에 있던 권한 허용 UI를 모듈화함.
    그리고 hilt를 통해 viewModel를 주입받도록 개선

--- app/src/main/java/com/example/notimanager/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/notimanager/presentation/ui/activity/MainActivity.kt
@@ -3,24 +3,19 @@ package com.example.notimanager.presentation.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.lifecycle.ViewModelProvider
 import com.example.notimanager.presentation.stateholder.viewmodel.NotificationPermissionViewModel
-import com.example.notimanager.presentation.ui.component.PermissionDialog
+import com.example.notimanager.presentation.ui.component.PermissionCheck
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private lateinit var viewModel: NotificationPermissionViewModel
+    private val viewModel: NotificationPermissionViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        viewModel = ViewModelProvider(this)[NotificationPermissionViewModel::class.java]
-        viewModel.checkNotificationPermission()
 
         setContent {
             MainContent()
@@ -34,14 +29,6 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MainContent() {
-        val isPermissionGranted by viewModel.isPermissionGranted.observeAsState()
-
-        if (isPermissionGranted == false) {
-            PermissionDialog.SimplePermissionDialog(
-                title = "알림 권한 필요",
-                message = "알림을 받기 위해 알림 권한이 필요합니다.",
-                onAllow = { viewModel.requestPermission(this) },
-            )
-        }
+        PermissionCheck(viewModel)
     }
 }
