60477a5edddf0f99e56ffd79b0982c372b9fcd9b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-22 13:48:36 +0900

    build: room 플러그인 추가
    db 스키마 이전을 위해 room 플러그인 추가
    minSDK 수정 -> resource id를 위해.
    room에서 생성한 스키마 파일 gitignore에 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("androidx.room")
     id("org.jetbrains.kotlinx.kover")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
@@ -13,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.notimanager"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -53,6 +54,10 @@ android {
             }
 
         }
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
 }
 
@@ -117,5 +122,6 @@ dependencies {
 
     // hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.android.compiler)
 }
