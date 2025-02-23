a26604078ebe0c05dc22b29ccf1e904822725932
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-23 10:40:17 +0900

    Test: domain useCase 테스트 추가
    테스터블하도록 함수 개선

---
+++ app/src/test/java/com/example/notimanager/domain/usecase/NotificationTitleUseCaseTest.kt
@@ -0,0 +1,32 @@
+package com.example.notimanager.domain.usecase
+
+import com.example.notimanager.domain.model.NotificationTitle
+import com.example.notimanager.domain.repository.NotificationRepositoryDomainInterface
+import com.example.notimanager.utils.TestUtils.readJsonFile
+import com.example.notimanager.utils.TestUtils.toDtoList
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+
+class NotificationTitleUseCaseTest: BehaviorSpec({
+    val repository = mockk<NotificationRepositoryDomainInterface>()
+    val useCase = NotificationTitleUseCase(repository)
+
+    val layer = "domain"
+    val target = "model/NotificationTitle.json"
+    Context("NotificationTitleUseCase 테스트"){
+        Given("특정 앱의 알림 제목 리스트가 필요한 경우 "){
+            val notificationTitleList = readJsonFile(layer, target) toDtoList NotificationTitle::class.java
+
+            When("getNotificationTitleList를 호출하며"){
+                coEvery { repository.getNotificationTitleList(any(), any())  } returns notificationTitleList
+
+                Then("notificationTitleList를 반환해야 한다."){
+                    val result = useCase.getNotificationTitleList("", "")
+                    result shouldBe notificationTitleList
+                }
+            }
+        }
+    }
+})
