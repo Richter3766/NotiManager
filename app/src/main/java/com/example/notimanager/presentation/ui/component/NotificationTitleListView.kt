f81bbbc09b1682e2f4d82fc26b9dbd14add0422c
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-25 10:35:12 +0900

    feat: 앱 제목 리스트 화면 UI
    메인화면과 같은 리스트 아이템 뷰
    topbar 추가

--- app/src/main/java/com/example/notimanager/presentation/ui/component/NotificationTitleListView.kt
+++ app/src/main/java/com/example/notimanager/presentation/ui/component/NotificationTitleListView.kt
@@ -2,19 +2,25 @@ package com.example.notimanager.presentation.ui.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.notimanager.common.objects.DateFormatter.formatTimestamp
@@ -23,10 +29,12 @@ import com.example.notimanager.presentation.stateholder.state.NotificationTitleS
 import com.example.notimanager.presentation.stateholder.viewmodel.NotificationTitleViewModel
 
 @Composable
-fun NotificationTitleListView(navController: NavController, viewModel: NotificationTitleViewModel) {
+fun NotificationTitleListView(navController: NavController, innerPadding: PaddingValues,viewModel: NotificationTitleViewModel) {
     val notificationTitleState by viewModel.notificationTitleState.observeAsState(NotificationTitleState())
 
-    Column {
+    Column (
+        Modifier.padding(innerPadding)
+    ){
         if (notificationTitleState.isLoading) {
             CircularProgressIndicator(modifier = Modifier.padding(16.dp))
         } else if (notificationTitleState.error != null) {
@@ -55,10 +63,24 @@ fun NotificationTitleItemView(notification: NotificationTitle, onClick: () -> Un
         verticalAlignment = Alignment.CenterVertically
     ) {
         AppIconView(notification.notificationIcon)
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            BasicText(text = notification.title, style = MaterialTheme.typography.bodyLarge)
-            BasicText(text = notification.content, style = MaterialTheme.typography.bodyMedium)
-            BasicText(text = formatTimestamp(notification.timestamp), style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = notification.title,
+                style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+
+            )
+            Text(
+                text = notification.content,
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = formatTimestamp(notification.timestamp),
+                style = MaterialTheme.typography.labelSmall,
+                color = Color.LightGray
+            )
         }
     }
 }
--- app/src/main/java/com/example/notimanager/presentation/ui/component/TopAppBar.kt
+++ app/src/main/java/com/example/notimanager/presentation/ui/component/TopAppBar.kt
@@ -1,6 +1,7 @@
 package com.example.notimanager.presentation.ui.component
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,6 +19,29 @@ fun MainTopAppBar(){
     TopAppBar(
         title = {
             Text(text = "Notification Manager")
+        },
+        actions = {
+            IconButton(onClick = { /* TODO: 클릭 이벤트 처리 */ }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "더 보기")
+            }
+        })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TitleTopAppBar(title: String, onBackClick: () -> Unit){
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "뒤로 가기")
+            }
         },
         actions = {
             IconButton(onClick = { /* TODO: 클릭 이벤트 처리 */ }) {
