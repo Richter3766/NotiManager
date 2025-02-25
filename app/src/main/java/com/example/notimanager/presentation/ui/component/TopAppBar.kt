0a77b27e720bd66989a93eff8954827db2b755e5
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-25 10:25:58 +0900

    feat: 메인 화면 UI 개선
    아이콘 크기 조정
    텍스트 크기 조정
    아이콘 텍스트 간 간격 조정

---
+++ app/src/main/java/com/example/notimanager/presentation/ui/component/TopAppBar.kt
@@ -0,0 +1,34 @@
+package com.example.notimanager.presentation.ui.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopAppBar(){
+    TopAppBar(
+        title = {
+            Text(text = "Notification Manager")
+        },
+        actions = {
+            IconButton(onClick = { /* TODO: 클릭 이벤트 처리 */ }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "더 보기")
+            }
+        })
+}
+
+@Preview(backgroundColor = 1)
+@Composable
+fun PreviewMainTopAppBar(){
+    MaterialTheme{
+        MainTopAppBar()
+    }
+}
