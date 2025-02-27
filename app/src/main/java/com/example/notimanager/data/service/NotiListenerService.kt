96c9c7a78721930851a9ecfb7816819f8df0b2fd
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-02-27 10:24:26 +0900

    feat: 컬러풀 앱 아이콘 로직
    smallIcon에 색을 입힘으로써 앱 아이콘 표현.
    이후 패키지 앱 아이콘을 가져오는 방식과 비교해볼 예정
    일종의 테스트 커밋.

--- app/src/main/java/com/example/notimanager/data/service/NotiListenerService.kt
+++ app/src/main/java/com/example/notimanager/data/service/NotiListenerService.kt
@@ -3,24 +3,29 @@ package com.example.notimanager.data.service
 import android.graphics.drawable.Icon
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import com.example.notimanager.data.model.AppIconModel
 import com.example.notimanager.data.model.NotificationIconModel
 import com.example.notimanager.data.model.NotificationMetaModel
 import com.example.notimanager.data.model.NotificationModel
 import com.example.notimanager.data.repository.NotificationRepositoryInterface
 import com.example.notimanager.data.utils.AppIconGetter.convertByteArray
+import com.example.notimanager.data.utils.AppIconGetter.convertByteArrayWithColor
 import com.example.notimanager.data.utils.NameGetter
 import com.example.notimanager.data.utils.IntentHelper
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class NotiListenerService: NotificationListenerService() {
     @Inject
     lateinit var notificationRepository: NotificationRepositoryInterface
+    private val mutex = Mutex()
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         sbn?.let {
@@ -28,23 +33,19 @@ class NotiListenerService: NotificationListenerService() {
             val postTime = it.postTime
             val appName = NameGetter.getAppName(this, it)
 
-            val title = notification.extras.getString("android.title") ?: ""
-            val content = notification.extras.getString("android.text") ?: ""
+            val title = notification.extras.getString("android.title")
+            val content = notification.extras.getString("android.text")
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val id = insertNotification(appName, title, content, postTime)
-
-                launch {
-                    insertNotificationMeta(id, notification.contentIntent.creatorPackage)
+            if (title != null && content != null){
+                CoroutineScope(Dispatchers.IO).launch {
+                    mutex.withLock {
+                        val id = insertNotification(appName, title, content, postTime)
+                        insertNotificationMeta(id, notification.contentIntent.creatorPackage)
+                        insertNotificationIcon(id, notification.getLargeIcon())
+                        insertAppIcon(appName, notification.smallIcon, notification.color)
+                    }
                 }
 
-                launch {
-                    insertNotificationIcon(id, notification.getLargeIcon())
-                }
-
-                launch {
-                    insertAppIcon(appName, notification.smallIcon)
-                }
             }
         }
     }
@@ -90,9 +91,10 @@ class NotiListenerService: NotificationListenerService() {
 
     private suspend fun insertAppIcon(
         appName: String,
-        icon: Icon?
+        icon: Icon?,
+        color: Int
     ){
-        val byteArray = convertByteArray(this@NotiListenerService, icon)
+        val byteArray = convertByteArrayWithColor(this@NotiListenerService, icon, color)
         val appIconModel = AppIconModel(
             notiAppName = appName,
             iconBytes = byteArray
