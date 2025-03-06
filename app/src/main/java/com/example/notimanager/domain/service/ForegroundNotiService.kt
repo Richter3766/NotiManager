bd52bf368bf08bc71531ccccbfa29bd88da9a5a8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-03-06 12:02:26 +0900

    feat: 포그라운드 서비스 구현
    androidManifest에 포그라운드 서비스 등록
    해당 서비스에서 받은 알림을 상태바에서 보여줌.

---
+++ app/src/main/java/com/example/notimanager/domain/service/ForegroundNotiService.kt
@@ -0,0 +1,93 @@
+package com.example.notimanager.domain.service
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.IBinder
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.notimanager.R
+import com.example.notimanager.presentation.ui.activity.MainActivity
+
+class ForegroundNotiService: Service() {
+    private val channelId = "NotiManagerChannel"
+    private val groupId = "NotiManagerGroup"
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val clearGroup = intent?.extras?.getBoolean("clearGroup") ?: false
+        if (clearGroup) clearGroupNotifications()
+
+        val appName = intent?.extras?.getString("appName") ?: ""
+        val content = intent?.extras?.getString("content") ?: ""
+        val isGroupSummary = intent?.extras?.getBoolean("isGroupSummary") ?: false
+        putNotification(appName, content, isGroupSummary)
+
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopForeground(STOP_FOREGROUND_REMOVE)
+    }
+
+    private fun putNotification(
+        appName: String,
+        content: String,
+        isGroupSummary: Boolean) {
+        val notificationIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("appName", appName)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(appName)
+            .setContentText(content)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setGroup(groupId)
+            .setAutoCancel(true)
+            .setOngoing(true)
+            .setGroupSummary(isGroupSummary)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        NotificationManagerCompat.from(this).notify(appName.hashCode(), notification)
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            channelId,
+            "NotiManager Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+    }
+
+    private fun clearGroupNotifications() {
+        NotificationManagerCompat.from(this).cancelAll()
+    }
+}
