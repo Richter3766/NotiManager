d08355797973cbf1f265b54811f40c045e6b5bef
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-03-08 11:27:11 +0900

    feat: 모달 창 상자에 적절한 아이콘 추가
    모달창 각 행동에 맞는 아이콘을 텍스트 왼쪽에 놓아둠.

--- app/src/main/java/com/example/notimanager/presentation/ui/component/box/BoxView.kt
+++ app/src/main/java/com/example/notimanager/presentation/ui/component/box/BoxView.kt
@@ -3,8 +3,10 @@ package com.example.notimanager.presentation.ui.component.box
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,15 +24,16 @@ fun ClickableTextView(
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick)
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             icon?.invoke()
+            if(icon != null) Spacer(modifier = Modifier.width(16.dp))
             Text(
                 text = text,
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.align(Alignment.CenterVertically)
             )
         }
-
     }
 }
