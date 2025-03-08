b9ae1969e60a41540214d620913c0064baa7498c
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-03-08 17:39:04 +0900

    feat: 관리하지 않는 알림에 앱 아이콘 추가
    관리하지 않는 알림 목록에서 앱 아이콘을 추가함.
    필터링 리스트와 구분을 두기 위해 dto, 도메인 모델을 구분함.

--- app/src/main/java/com/example/notimanager/data/dto/FilteredNotificationDto.kt
+++ app/src/main/java/com/example/notimanager/data/dto/FilteredNotificationDto.kt
@@ -1,17 +1,20 @@
 package com.example.notimanager.data.dto
 
+import android.graphics.BitmapFactory
 import com.example.notimanager.domain.model.FilteredNotification
 
 data class FilteredNotificationDto(
     val id: Long,
     val appName: String,
     val title: String,
+    val iconBytes: ByteArray,
 ){
     fun toDomain(): FilteredNotification {
         return FilteredNotification(
             id = this.id,
             appName = this.appName,
             title = this.title,
+            appIcon = BitmapFactory.decodeByteArray(iconBytes, 0, iconBytes.size),
         )
     }
 }
--- app/src/main/java/com/example/notimanager/data/source/local/dao/FilteredNotificationDao.kt
+++ app/src/main/java/com/example/notimanager/data/source/local/dao/FilteredNotificationDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.notimanager.data.dto.FilteredListItemDto
 import com.example.notimanager.data.dto.FilteredNotificationDto
 import com.example.notimanager.data.model.FilteredNotificationModel
 
@@ -16,10 +17,12 @@ interface FilteredNotificationDao {
         SELECT id, appName, title FROM filtered_notification
         WHERE appName = :appName AND (title = "" OR title = :title OR title = :subText)
     """)
-    suspend fun getSpecificFilteredList(appName: String, title: String, subText: String): List<FilteredNotificationDto>
+    suspend fun getSpecificFilteredList(appName: String, title: String, subText: String): List<FilteredListItemDto>
 
     @Query("""
-        SELECT id, appName, title FROM filtered_notification
+        SELECT id, appName, title, ai.iconBytes
+        FROM filtered_notification AS fn
+        INNER JOIN app_icon AS ai ON fn.appName = ai.notiAppName  
         ORDER BY appName ASC
     """)
     suspend fun getFilteredList(): List<FilteredNotificationDto>
