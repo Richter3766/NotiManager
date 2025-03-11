2a85019237d6124162e894a0f10f2dda0225f036
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-03-11 18:07:23 +0900

    feat: 네이티브 ads 추가 구현
    당장 활용할 생각은 없지만,
    추후 재사용을 위한 커밋

--- app/src/main/java/com/example/notimanager/presentation/ui/ads/NotiNativeAds.kt
+++ app/src/main/java/com/example/notimanager/presentation/ui/ads/NotiNativeAds.kt
@@ -1,20 +1,61 @@
 package com.example.notimanager.presentation.ui.ads
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Badge
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.notimanager.R
+import com.example.notimanager.common.objects.DateFormatter.toBitmap
+import com.example.notimanager.presentation.ui.component.common.AppIconView
 import com.google.android.gms.ads.nativead.NativeAd
 
 @Composable
 fun NotiNativeAds(nativeAd: NativeAd) {
     NotiNativeAdView(ad = nativeAd) { ad, view ->
-        //Style it whatever you want
-        Log.i("ad 정보", ad.toString())
-        Log.i("ad 정보", ad.headline.toString())
-        Column {
-            ad.headline?.let { Text(text = it) }
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            AppIconView(ad.icon?.drawable?.toBitmap())
+            Column {
+                // 광고 뱃지와 함께 광고 제목
+                Row(){
+                    Image(
+                        painter = painterResource(id = R.drawable.ad_badge),
+                        contentDescription = "ad badge",
+                        modifier = Modifier.size(16.dp)
+                    )
+                    ad.headline?.let {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold),
+                        )
+                    }
+                }
+                ad.body?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.bodySmall,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
             TextButton(
                 content = { ad.callToAction?.let { Text(text = it) } },
                 onClick = { view.performClick() },
