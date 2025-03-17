82b7f9bca0d6362b53d71d8b848f6616d36b2e30
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-03-17 14:05:18 +0900

    build: 버전 증가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.notimanager.notimanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "admob_app_id", System.getenv("ADMOB_APP_ID") ?: "")
         resValue("string", "admob_banner_id", System.getenv("ADMOB_BANNER_ID") ?: "")
